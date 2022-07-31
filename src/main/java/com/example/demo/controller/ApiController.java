ed8861cf26523098f264e6eebd347c277b980f02
ishwang <his8457@naver.com>
2022-07-31 18:29:59 +0900

    test API 를 추가하고 index 페이지로 attribute를 전달하는 코드 작성

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,11 @@ public class ApiController {
     @GetMapping("/hello")
     public String hello(){
         return "heelo spring boot";
+    }
+
+    @GetMapping("/test")
+    public String test(@ModelAttribute ModelMap map){
+        map.addAttribute("firstName", "hwang");
+        return "index";
     }
 }
