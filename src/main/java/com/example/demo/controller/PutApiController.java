bf6cbc8c0eac4de8eccf1e07d85f164c94ff2510
ishwang <his8457@naver.com>
2022-08-04 23:03:09 +0900

    #3-Put API 만들기
    commit 후 push 하면서 파일이 unstaging 되어 다시 commit

---
+++ src/main/java/com/example/demo/controller/PutApiController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.PutRequestDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/put-api")
+public class PutApiController {
+
+    @PutMapping("/put")
+    public void put (@RequestBody PutRequestDto request){
+        System.out.println(request.toString());
+    }
+
+    @PutMapping("/put-object")
+    public PutRequestDto putObj (@RequestBody PutRequestDto request){
+        return request;
+    }
+
+    @PutMapping("/put/{userId}")
+    public PutRequestDto putGetParam (@RequestBody PutRequestDto request
+                                    , @PathVariable(name = "userId") Long userId){
+        System.out.println(userId);
+        return request;
+    }
+}
